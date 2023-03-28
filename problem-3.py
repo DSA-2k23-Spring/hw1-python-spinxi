b5c5ce65f864c01daa1ff6b40b12b129c0d68937
spinxi <50767516+spinxi@users.noreply.github.com>
2023-03-28 19:04:56 +0400

    Update problem-3.py

--- problem-3.py
+++ problem-3.py
@@ -1,6 +1,7 @@
-otxobiti = "23"
-atobiti = 0
-for i, digit in enumerate(reversed(otxobiti)):
-    #როგორც ფორმულა MNOPQ = QxB^0 + PxB^1 + O x B^2 + N x B^3 + M x B^4 B=რომელ სისტემაშიც გადადის
-    atobiti += int(digit) * 4 ** i
-print(atobiti) # Output: 11
+def otx(strr):
+    otxobiti = strr
+    atobiti = 0
+    for i, digit in enumerate(reversed(otxobiti)):
+        atobiti += int(digit) * 4 ** i
+    return atobiti
+print(otx("12121212"))
